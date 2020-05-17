5a045a478adfa3f8fce1e8e755a4d7ad9d54139f
Nikolov0080 <apofis12343@gmail.com>
2020-05-17 21:58:08 +0300

    dsdsdsd

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+  
+
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>{this.props.instagram}</li>
+          <li>{this.props.facebook}</li>
+          <li>{this.props.twitter}</li>
+          <li>{this.props.maikati}</li>
+          
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
