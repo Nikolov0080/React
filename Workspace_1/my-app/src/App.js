9739e83b6a26be1b1b20dd73373fb112d5efcac5
Nikolov0080 <apofis12343@gmail.com>
2020-05-25 18:38:17 +0300

    some scripts added

--- Workspace_1/my-app/src/App.js
+++ Workspace_1/my-app/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>h1 text</h1>
+      <Person></Person>
     </div>
   );
+
+
+  // return React.createElement('div', { className: 'App' }, null, React.createElement('h1', null, 'haaa'));
 }
 
 export default App;
