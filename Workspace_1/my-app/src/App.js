7ae50619bba7bc761cdd1758dd5d7fa19d6e6e74
Nikolov0080 <apofis12343@gmail.com>
2020-05-25 20:54:28 +0300

    updated

--- Workspace_1/my-app/src/App.js
+++ Workspace_1/my-app/src/App.js
@@ -6,8 +6,12 @@ import Person from './Person/person';
 function App() {
   return (
     <div className="App">
-      <h1>h1 text</h1>
-      <Person  />
+      <h1>Random names and ages</h1>
+      <Person name="George" age="23">Hobby fishing alone... forever alone</Person>
+      <Person  name="Adi" age="12"/>
+      <Person  name="Azis" age="55"/>
+      <Person  name="Minka" age="36"/>
+
     </div>
   );
 
--- Workspace_1/my-app/src/Person/person.js
+++ Workspace_1/my-app/src/Person/person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => {
-   return <p>Person age is {Math.floor(Math.random() * (333-6))}</p>
+const person = (props) => {
+    return (
+        <div>
+            <p>{props.name} age is {props.age}</p>
+            <p>{props.children} </p>
+        </div>
+    )
 }
 
 export default person;
