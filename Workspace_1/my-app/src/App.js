1a62a9458bf6596a530282d0e14b5bb14c579fa5
Nikolov0080 <apofis12343@gmail.com>
2020-05-25 21:34:15 +0300

    update page SPA

--- Workspace_1/my-app/src/App.js
+++ Workspace_1/my-app/src/App.js
@@ -5,17 +5,36 @@ import Person from './Person/person';
 
 class App extends Component {
 
+  state = {
+    persons: [
+      { name: "George", age: 45 },
+      { name: "Dragan", age: 32 },
+      { name: "Ivan", age: 21 },
+    ]
+  }
 
+  switchNameHandler = () => {
+
+    this.setState({
+      persons: [
+        { name: "John Doe", age: 45 },
+        { name: "Dragan", age: 32 },
+        { name: "Ivan", age: 21 },
+      ]
+    }
+    );
+    // NOT LIKE THIS:  this.state.persons[0].name = 'NO!'
+    // console.log('test ');
+  }
 
   render() {
     return (
       <div className="App" >
         <h1>Random names and ages</h1>
-        <button>Switch Name</button>
-        <Person name="George" age="23">Hobby fishing alone... forever alone</Person>
-        <Person name="Adi" age="12" />
-        <Person name="Azis" age="55" />
-        <Person name="Minka" age="36" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobby fishing alone... forever alone</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
 
       </div>
     );
