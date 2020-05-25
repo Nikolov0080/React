f3f80e2742963befec422fb9cdb028594541d9bd
Nikolov0080 <apofis12343@gmail.com>
2020-05-25 21:01:06 +0300

    main app from func to class

--- Workspace_1/my-app/src/App.js
+++ Workspace_1/my-app/src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/person';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Random names and ages</h1>
-      <Person name="George" age="23">Hobby fishing alone... forever alone</Person>
-      <Person  name="Adi" age="12"/>
-      <Person  name="Azis" age="55"/>
-      <Person  name="Minka" age="36"/>
+class App extends Component {
 
-    </div>
-  );
+
+
+  render() {
+    return (
+      <div className="App" >
+        <h1>Random names and ages</h1>
+        <button>Switch Name</button>
+        <Person name="George" age="23">Hobby fishing alone... forever alone</Person>
+        <Person name="Adi" age="12" />
+        <Person name="Azis" age="55" />
+        <Person name="Minka" age="36" />
+
+      </div>
+    );
+  }
 
 
   // return React.createElement('div', { className: 'App' }, null, React.createElement('h1', null, 'haaa'));
