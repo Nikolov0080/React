3b85d525c6e5a342f7eb3c1381e039bc4e7a058d
Nikolov0080 <apofis12343@gmail.com>
2020-05-26 16:20:28 +0300

    class replaced with a arrow func

--- Workspace_1/my-app/src/App.js
+++ Workspace_1/my-app/src/App.js
@@ -1,47 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/person';
 
 
-class App extends Component {
-
-  state = {
-    persons: [
-      { name: "George", age: 45 },
-      { name: "Dragan", age: 32 },
-      { name: "Ivan", age: 21 },
-    ]
-  }
-
-  switchNameHandler = () => {
-
-    this.setState({
+const App = props => {
+  const [personState, setPersons] = useState(
+    {
       persons: [
-        { name: "John Doe", age: 45 },
+        { name: "George", age: 45 },
         { name: "Dragan", age: 32 },
         { name: "Ivan", age: 21 },
       ]
     }
-    );
-    // NOT LIKE THIS:  this.state.persons[0].name = 'NO!'
-    // console.log('test ');
+  );
+
+  const switchNameHandler = () => {
+
+    setPersons({
+      persons: [
+        { name: "John Doe", age: 45 },
+        { name: "Minka ", age: 32 },
+        { name: "Shisho Bakshisho", age: 21 },
+      ]
+    })
   }
 
-  render() {
-    return (
-      <div className="App" >
-        <h1>Random names and ages</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobby fishing alone... forever alone</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+  return (
+    <div className="App" >
+      <h1>Random names and ages</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personState.persons[0].name} age={personState.persons[0].age}></Person>
 
-      </div>
-    );
-  }
+      <Person name={personState.persons[1].name} age={personState.persons[1].age}></Person>
+      <Person name={personState.persons[2].name} age={personState.persons[2].age}></Person>
+    </div>
+  );
 
-
-  // return React.createElement('div', { className: 'App' }, null, React.createElement('h1', null, 'haaa'));
 }
 
 export default App;
+
+  // state = {
+  //   persons: [
+  //     { name: "George", age: 45 },
+  //     { name: "Dragan", age: 32 },
+  //     { name: "Ivan", age: 21 },
+  //   ]
+  // }
+
+  // switchNameHandler = () => {
+
+  //   this.setState({
+  //     persons: [
+  //       { name: "John Doe", age: 45 },
+  //       { name: "Dragan", age: 32 },
+  //       { name: "Ivan", age: 21 },
+  //     ]
+  //   }
+  //   );
