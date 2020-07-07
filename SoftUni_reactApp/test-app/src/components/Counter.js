c840b2972cfb5396426ce41901d78bdbcd40e9b8
Nikolov0080 <apofis12343@gmail.com>
2020-07-07 23:03:35 +0300

    changed src/components/counter

--- SoftUni_reactApp/test-app/src/components/Counter.js
+++ SoftUni_reactApp/test-app/src/components/Counter.js
@@ -3,9 +3,16 @@ import { Button } from 'react-bootstrap';
 
 class Counter extends Component {
 
-    state = {
-        counter: this.props.counter
+    static getDerivedStateFromProps(props, state) {
+        state = {
+            counter: this.props.counter
+        }
     }
+
+    componentDidMount() {
+
+    }
+
 
     addOne = () => {
         this.setState({
@@ -44,7 +51,6 @@ class Counter extends Component {
                 <Button variant="outline-success" onClick={this.addTen}>Add 10</Button>
                 <Button variant={this.state.counter >= 10 ? "outline-warning" : "outline-secondary"} onClick={this.removeTen}>Remove 10</Button>
                 <Button variant={this.state.counter >= 1 ? "outline-warning" : "outline-secondary"} onClick={this.removeOne}>Remove 1</Button>
-             
             </div>
         )
     }
