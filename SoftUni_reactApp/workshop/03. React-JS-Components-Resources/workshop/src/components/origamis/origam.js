6b15ed2f13ce39d58921360bd5e57fd4763a9fac
Nikolov0080 <apofis12343@gmail.com>
2020-07-13 22:42:10 +0300

    changed src/components/origamis

--- SoftUni_reactApp/workshop/03. React-JS-Components-Resources/workshop/src/components/origamis/origam.js
+++ SoftUni_reactApp/workshop/03. React-JS-Components-Resources/workshop/src/components/origamis/origam.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import style from './index.module.css';
 
+const image = "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
 
 const Origam = ({ description, author }) => {
     return (
         <div className={style.Post}>
-                <img className={style.img}></img>
+            <img src={image} alt="ajhsjkahskj" className={style.img} />
             <p className={style.description}>
                 {description}
             </p>
-
-         
-     
 
             <div>
                 <span className={style.user}>{author.username}</span>
